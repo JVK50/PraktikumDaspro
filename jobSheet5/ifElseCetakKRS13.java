39d9c8b3d81e6bb5589dac171a809677d97f84e7
JVK50 <jidanali32@gmail.com>
2025-10-02 10:40:55 +0700

    Tugas 1

--- jobSheet5/ifElseCetakKRS13.java
+++ jobSheet5/ifElseCetakKRS13.java
@@ -4,16 +4,10 @@ public class ifElseCetakKRS13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("--- Cetak KRS SIAKAD ---");
-
-        System.out.println("Masukkan semester: ");
-        int semester = sc.nextInt();
-
-        if (semester < 9 ) {
-            System.out.println("KRS semester " + semester + " ditampilkan");
-        }
-        else {
-            System.out.println("Semester tidak valid");
-        }
+        System.out.println("Apakah UKT sudah lunas? (Ya/Tidak): ");
+        boolean uktLunas = sc.nextBoolean();
+        String ukt = uktLunas ? "Pembayaran UKT sudah lunas" : "registrasi ditolak";
+        System.out.println(ukt);
         sc.close();
     }
 }
